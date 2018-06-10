e52baa1015dba2b002bcc8e6fc76aec71a83d180
odan <odan3240@gmail.com>
2018-06-10 23:17:13 +0900

    add package block

--- lib/Vector.pm
+++ lib/Vector.pm
@@ -3,44 +3,44 @@ use warnings;
 use v5.18;
 
 
-package Vector;
-sub new {
-    my $class = shift;
+package Vector {
+    sub new {
+        my $class = shift;
 
-    my $self = bless {
-        _data => []
-    }, $class;
-    return $self;
-}
+        my $self = bless {
+            _data => []
+        }, $class;
+        return $self;
+    }
 
-sub push_back {
-    my ( $self, $x ) = @_;
-    my $data = $self->{_data};
-    push @$data => $x;
-}
+    sub push_back {
+        my ( $self, $x ) = @_;
+        my $data = $self->{_data};
+        push @$data => $x;
+    }
 
-sub size {
-    my $self = shift;
-    my $data = $self->{_data};
-    scalar(@$data);
-}
+    sub size {
+        my $self = shift;
+        my $data = $self->{_data};
+        scalar(@$data);
+    }
 
-sub at {
-    my ( $self, $i ) = @_;
-    my $data = $self->{_data};
-    my $len = $self->size;
-    die "invalid index: $i (size = $len)\n" unless 0 <= $i && $i < $self->size;
-    $data->[$i];
-}
+    sub at {
+        my ( $self, $i ) = @_;
+        my $data = $self->{_data};
+        my $len = $self->size;
+        die "invalid index: $i (size = $len)\n" unless 0 <= $i && $i < $self->size;
+        $data->[$i];
+    }
 
-sub front {
-    my $self = shift;
-    $self->at(0);
-}
+    sub front {
+        my $self = shift;
+        $self->at(0);
+    }
 
-sub back {
-    my $self = shift;
-    $self->at($self->size - 1);
-}
-
+    sub back {
+        my $self = shift;
+        $self->at($self->size - 1);
+    }
+};
 1;
