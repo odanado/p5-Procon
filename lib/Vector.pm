62e339f5183e3276969a229fdb4f569a5b2de5d2
Kita Tomoya <kita@mfac.jp>
2018-06-27 22:11:11 +0900

    vectorにpop_backを実装した

--- lib/Vector.pm
+++ lib/Vector.pm
@@ -2,24 +2,22 @@ use strict;
 use warnings;
 use v5.18;
 
-
 package Vector {
+
     sub new {
         my $class = shift;
-        my ($size, $value) = @_;
+        my ( $size, $value ) = @_;
 
         my $data;
-        if (defined $size) {
+        if ( defined $size ) {
             $value = 0 unless defined $value;
-            $data = [ map { $value } 1..$size ];
+            $data = [ map { $value } 1 .. $size ];
         }
         else {
             $data = [];
         }
 
-        my $self = bless {
-            _data => $data
-        }, $class;
+        my $self = bless { _data => $data }, $class;
         return $self;
     }
 
@@ -27,6 +25,12 @@ package Vector {
         my ( $self, $x ) = @_;
         my $data = $self->{_data};
         push @$data => $x;
+    }
+
+    sub pop_back {
+        my $self = shift;
+        my $data = $self->{_data};
+        pop @$data;
     }
 
     sub size {
@@ -39,10 +43,11 @@ package Vector {
         my ( $self, $i, $x ) = @_;
 
         my $data = $self->{_data};
-        my $len = $self->size;
-        die "invalid index: $i (size = $len)\n" unless 0 <= $i && $i < $self->size;
+        my $len  = $self->size;
+        die "invalid index: $i (size = $len)\n"
+          unless 0 <= $i && $i < $self->size;
 
-        if (defined $x) {
+        if ( defined $x ) {
             $data->[$i] = $x;
         }
         else {
@@ -57,7 +62,7 @@ package Vector {
 
     sub back {
         my $self = shift;
-        $self->at($self->size - 1);
+        $self->at( $self->size - 1 );
     }
 };
 1;
